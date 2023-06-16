377de8fc27da1a897e87afed3e8fb479c2a1624b
Alexandre Choplin <a.choplin@alienor.net>
2023-06-16 10:27:39 +0200

    [PRISMA] DB setup correction

--- prisma/migrations/20230615142002_reset1/migration.sql
+++ prisma/migrations/20230615142002_reset1/migration.sql
@@ -25,6 +25,3 @@ CREATE TABLE `Session` (
     UNIQUE INDEX `Session_sid_key`(`sid`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ prisma/migrations/20230616082632_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `session` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `session` DROP COLUMN `userId`;
