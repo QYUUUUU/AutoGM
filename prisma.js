24b391996567c8ddeb1d22114324f659526c4654
QYUUUUU <achoplin@gmail.com>
2026-03-29 21:31:54 +0200

    fix(docker/prisma): make things work with mysql, docker alpine node, env issues, readme update

--- prisma.js
+++ prisma.js
@@ -1,4 +1,11 @@
 import { PrismaClient } from '@prisma/client';
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.local`, override: true });
+
+const pool = mysql.createPool(process.env.DATABASE_URL);
+
 
 const prisma = new PrismaClient();
 
