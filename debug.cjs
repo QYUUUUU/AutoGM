1ab0204a2a1fa4731c3509bd9cfa35357041c1c9
QYUUUUU <achoplin@gmail.com>
2026-04-08 09:15:18 +0200

    [CLEAN] Leftover files deletion

--- debug.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('app/views/index.html.twig', 'utf-8');
-console.log("rituels-tab link present:", content.includes('id="rituels-tab"'));
-console.log("rituels-tab-pane present:", content.includes('id="rituels-tab-pane"'));
-console.log("Number of tab-pane div:", (content.match(/class="tab-pane /g) || []).length);
-console.log("Number of nav-link a tags:", (content.match(/class="nav-link"/g) || []).length);
--- debug2.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('app/views/index.html.twig', 'utf-8');
-const links = [...content.matchAll(/id="(.*?)[-]?tab"\s+data-bs-toggle="tab"/g)].map(m=>m[1]);
-console.log("Tab links:", links);
-const panes = [...content.matchAll(/id="(.*?)[-]?tab-pane"/g)].map(m=>m[1]);
-console.log("Tab panes:", panes);
