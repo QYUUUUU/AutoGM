f8f6dff6acabebd08bb665e7c6d8997f697b2c38
QYUUUUU <achoplin@gmail.com>
2026-08-12 12:40:49 +0200

    Clean up vibe-coded changes

--- app/patch_faveurs_route2.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let routesCode = fs.readFileSync('routes/userRoutes.js', 'utf8');
-
-routesCode = routesCode.replace('faveurs = await prisma.faveur.findMany();', "faveurs = await prisma.faveur.findMany({ orderBy: [{ domaine: 'asc' }, { nom: 'asc' }] });");
-
-fs.writeFileSync('routes/userRoutes.js', routesCode);
-console.log("Updated route for sorting");
